c12f65304136be116c15736665e6a979eab2e4b2
Durran0490 <aleksands_riga@yaho.com>
2019-09-25 14:09:18 +0300

    2.1: added multiplication and division priority

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -12,14 +12,15 @@ public class Calculator {
         String a = expression[0];
         String op1 = expression[1];
         String b = expression[2];
-        String op2 = expression[3];
-        String c = expression[4];
 
         var result = calc(a, op1, b);
 
         if(expression.length ==5) {
 
-            if (op2 == "/" ){
+            String op2 = expression[3];
+            String c = expression[4];
+
+            if (op2.equals("/")){
                 result = calc(b, op2, c);
                 result = calc(a, op1, Double.toString(result));
             }else if (op2 == "*" ) {
